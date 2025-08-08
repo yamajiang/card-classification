eddb5ac1edad2f4d36e52684d25298d92ab85470
Yama <yamajiang.cs@gmail.com>
2025-08-08 14:32:33 -0400

    datasets and dataloader

--- train-cards.py
+++ train-cards.py
@@ -19,7 +19,7 @@ from tqdm.notebook import tqdm
 #print('Numpy version', np.__version__)
 #print('Pandas version', pd.__version__)
 
-#datasets and dataloader
+#datasets 
 class PlayingCardDataset(Dataset):
     #tells class what to do when its created
     def __init__(self, data_dir, transform=None):
@@ -41,5 +41,39 @@ dataset= PlayingCardDataset(data_dir= 'train')
 #testing len function
 print(len(dataset)) #7624
 #testing get item function 
-print(dataset[5]) 
+print(dataset[1]) 
+#displayes image with label
+image, label= dataset[10]
+plt.imshow(image)
+plt.title(f"Label: {label}")
+plt.axis('off')
+plt.show()
 
+#get dictionary associating target values with folder names
+data_dir = 'train'
+target_to_class = {v: k for k, v in ImageFolder(data_dir).class_to_idx.items()}
+print(target_to_class)
+
+#make sure images are the same size
+transform=transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor()
+])
+
+#prints tensor(data structure used to store data) of an image 
+data_dir='train'
+dataset= PlayingCardDataset(data_dir, transform)
+print(dataset[100])
+
+#iterate over dataset:
+#for image, label in dataset:
+
+#dataloader
+#random data pull of size 32 (batch size must be in power of twos (16, 32, 64,...))
+#no need to shuffle on test or validation set
+dataloader= DataLoader(dataset, batch_size=32, shuffle=True)
+
+for images, labels in dataloader: 
+    break
+
+print(images.shape, labels.shape)
